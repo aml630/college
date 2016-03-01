a03d4fb448e35d4336f2576f1f9b7846b4e493dd
alex <alex.m.larson@gmail.com>
2016-03-01 14:07:24 -0800

    starting front end userablility

--- Module/HomeModule.cs
+++ Module/HomeModule.cs
@@ -1,81 +1,28 @@
-// using System;
-// using System.Collections.Generic;
-// using Nancy;
-// using Nancy.ViewEngines.Razor;
-//
-// namespace ToDoList
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ => {
-//         return View["index.cshtml"];
-//       };
-//       Get["/tasks"] = _ => {
-//         List<Task> AllTasks = Task.GetAll();
-//         return View["tasks.cshtml", AllTasks];
-//       };
-//       Get["/categories"] = _ => {
-//         List<Category> AllCategories = Category.GetAll();
-//         return View["categories.cshtml", AllCategories];
-//       };
-//       //Create a new task
-//       Get["/tasks/new"] = _ => {
-//         return View["tasks_form.cshtml"];
-//       };
-//       Post["/tasks/new"] = _ => {
-//         Task newTask = new Task(Request.Form["task-description"], 0);
-//         newTask.Save();
-//         return View["success.cshtml"];
-//       };
-//       //Create a new category
-//       Get["/categories/new"] = _ => {
-//         return View["categories_form.cshtml"];
-//       };
-//       Post["/categories/new"] = _ => {
-//         Category newCategory = new Category(Request.Form["category-name"]);
-//         newCategory.Save();
-//         return View["success.cshtml"];
-//       };
-//       Get["tasks/{id}"] = parameters => {
-//         Dictionary<string, object> model = new Dictionary<string, object>();
-//         Task SelectedTask = Task.Find(parameters.id);
-//         List<Category> TaskCategories = SelectedTask.GetCategories();
-//         List<Category> AllCategories = Category.GetAll();
-//         model.Add("task", SelectedTask);
-//         model.Add("taskCategories", TaskCategories);
-//         model.Add("allCategories", AllCategories);
-//         return View["task.cshtml", model];
-//       };
-//       Get["categories/{id}"] = parameters => {
-//         Dictionary<string, object> model = new Dictionary<string, object>();
-//         Category SelectedCategory = Category.Find(parameters.id);
-//         List<Task> CategoryTasks = SelectedCategory.GetTasks();
-//         List<Task> AllTasks = Task.GetAll();
-//         model.Add("category", SelectedCategory);
-//         model.Add("categoryTasks", CategoryTasks);
-//         model.Add("allTasks", AllTasks);
-//         return View["category.cshtml", model];
-//       };
-//       Post["task/add_category"] = _ => {
-//         Category category = Category.Find(Request.Form["category-id"]);
-//         Task task = Task.Find(Request.Form["task-id"]);
-//         task.AddCategory(category);
-//         return View["success.cshtml"];
-//       };
-//       Post["category/add_task"] = _ => {
-//         Category category = Category.Find(Request.Form["category-id"]);
-//         Task task = Task.Find(Request.Form["task-id"]);
-//         category.AddTask(task);
-//         return View["success.cshtml"];
-//       };
-//       Patch["tasks/{id}"] =parameters=> {
-//         Task newTask = Task.Find(parameters.id);
-//         newTask.UpdateChecked(Request.Form["checkbox"]);
-//         List<Task> AllTasks = Task.GetAll();
-//         return View["tasks.cshtml", AllTasks];
-//       };
-//     }
-//   }
-// }
+using System;
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace CollegeNameSpace
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        List<Student> allStudents = Student.GetAll();
+        return View["index.cshtml", allStudents];
+      };
+
+      Post["/"] =_=> {
+        Student newStudent = new Student(Request.Form["student"], Request.Form["studentDate"]);
+        newStudent.Save();
+        List<Student> newStudentList = new List<Student>{};
+        newStudentList.Add(newStudent);
+        List<Student> allStudents = Student.GetAll();
+        return View["index.cshtml", allStudents];
+      };
+
+    }
+  }
+}
